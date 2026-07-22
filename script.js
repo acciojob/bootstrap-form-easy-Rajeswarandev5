0105e06c3fbd2b8a8e668effc8681b1cd261df75
Rajeswarandev5 <rajeswaran.dev5@gmail.com>
2026-07-22 21:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const forms=document.querySelectorAll('.needs-validation');
+
+Array.from(forms).forEach(form => {
+	form.addEventListener('submit', event => {
+		if(!form.checkValidity()){
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		form.classList.add('was-validated')
+	},flase)
+})
